c37c187fa4274fced57d200583bc18fa31d4f17a
kounosukedesu <kounosuk0731@ezweb.ne.jp>
2023-01-18 21:47:16 +0900

    ✨ footerのコンポーネント化

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import styles from '/styles/Home.module.css'
+
+export const Footer = () => {
+    return (
+        <footer className={styles.center}>
+            <p>🄫 2023 Time to Code - A Progect Built with Next.js</p>
+        </footer>
+    )
+}
