73ea03baeafffc8c03de391bf920f0e23edfd354
kounosukedesu <kounosuk0731@ezweb.ne.jp>
2023-01-18 21:43:55 +0900

    ✨ navの要素

---
+++ pages/about-us.js
@@ -0,0 +1,11 @@
+
+const AboutUsPage = () => {
+    return (
+        <>
+            <h1>About us page </h1>
+            <p>Start the new year off right by spending New Year's Day in London. Check out the January sales at London's best department stores, listen to a concert at a church or cathedral, or get caught up on culture at a museum exhibition. 1 Jan</p>
+        </>
+    )
+}
+
+export default AboutUsPage;
