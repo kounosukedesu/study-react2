4c4e9db1a35bc6ee46b8f4f9ce9beab0e3a3050b
kounosukedesu <kounosuk0731@ezweb.ne.jp>
2023-01-18 21:47:38 +0900

    ✨ headerのコンポーネント化

---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export const Header = () => {
+    return(
+        <header>
+        <nav>
+          <Link href='/'>
+            <>Home</>
+          </Link>
+          <Link href='/events'>
+            <>Events</>
+          </Link>
+          <Link href='/about-us'>
+            <>About Us</>
+          </Link>
+        </nav>
+      </header>
+    )
+}
