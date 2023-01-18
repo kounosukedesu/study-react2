1833222b737546b1853ed7cb752f470dd1688665
kounosukedesu <kounosuk0731@ezweb.ne.jp>
2023-01-18 21:45:54 +0900

    ✨ それぞれのイベントページ

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const EventsCatPage = ({data, pageName}) => {
+    return (
+        <>
+            <h1>Event in {pageName} </h1>
+            <div>
+                {data.map(ev => (
+                    <Link key={ev.id} href={`/events/${ev.city}/${ev.id}`} >
+                    <Image width={300} height={300} alt={ev.title} src={ev.image} />
+                        <h2>{ev.title}</h2>
+                        <p>{ ev.description}</p>
+                    </Link>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default EventsCatPage;
+
+export async function getStaticPaths() {
+    const { events_categories } = await import('/data/data.json')
+    const allPaths = events_categories.map((ev) => {
+        return {
+            params: {
+                cat:ev.id.toString()
+            }
+        }
+    })
+    return {
+        paths: allPaths,
+        fallback: false
+        }
+}
+
+export async function getStaticProps(context) {
+    const id = context?.params.cat
+    const { allEvents } = await import('/data/data.json')
+    const data = allEvents.filter(ev => ev.city === id)
+    return {
+        props: { data, pageName:id }
+    }
+}
